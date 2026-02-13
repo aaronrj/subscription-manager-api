db9c2102cbb4f795eb016acdb8c89e68e0ca1659
Aaron Rodriguez <aaron2001rj@gmail.com>
2026-02-13 20:56:48 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/arodriguez/subscriptionmanager/SubscriptionManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.arodriguez.subscriptionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionManagerApiApplication.class, args);
+    }
+
+}
