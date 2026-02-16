0163a349fb3b31878338f560f91a0d45ff2e481c
Aaron Rodriguez <aaron2001rj@gmail.com>
2026-02-16 23:55:42 +0100

    Day 3 - Swagger integrated and error handling improved

---
+++ src/main/java/com/arodriguez/subscriptionmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.arodriguez.subscriptionmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(RuntimeException ex) {
+        return Map.of(
+                "error", ex.getMessage()
+        );
+    }
+}
